5670c17ad87356a06b8a482f8d876d0fc8776ec1
Andrew Palmer <atp@sdf.org>
2022-01-12 03:39:28 -0600

    Remove unneeded flags

--- include/jtypes/jtypes.h
+++ include/jtypes/jtypes.h
@@ -12,13 +12,13 @@
  */
 
 enum jtype {
-    JTYPE_INTEGER = 1 << 0,
-    JTYPE_NUMBER = 1 << 1,
-    JTYPE_STRING = 1 << 2,
-    JTYPE_ARRAY = 1 << 3,
-    JTYPE_OBJECT = 1 << 4,
-    JTYPE_BOOL = 1 << 5,
-    JTYPE_NULL = 1 << 6,
+    JTYPE_INTEGER,
+    JTYPE_NUMBER,
+    JTYPE_STRING,
+    JTYPE_ARRAY,
+    JTYPE_OBJECT,
+    JTYPE_BOOL,
+    JTYPE_NULL,
 };
 
 
