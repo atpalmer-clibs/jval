a878632a8f7904ea97146edd84cbbbf2321fee61
Andrew Palmer <atp@sdf.org>
2022-01-12 05:04:37 -0600

    Update API comments

--- include/jtypes/jtypes.h
+++ include/jtypes/jtypes.h
@@ -6,11 +6,6 @@
 #include <string.h>
 
 
-/*** jtype ***/
-/*
- * Type specification for jval
- */
-
 enum jtype {
     JTYPE_INTEGER,
     JTYPE_NUMBER,
@@ -21,10 +16,18 @@ enum jtype {
     JTYPE_NULL,
 };
 
-
-/*** jval ***/
 /*
- * Generic JSON value of any type
+ * jval struct represents any (JSON) value
+ * types have a corresponding union member
+ *
+ * jval.type        jval.value
+ * JTYPE_INTEGER    .as_long
+ * JTYPE_NUMBER     .as_double
+ * JTYPE_STRING     .as_string
+ * JTYPE_ARRAY      .as_container
+ * JTYPE_OBJECT     .as_container
+ * JTYPE_BOOL       .as_bool
+ * JTYPE_NULL       <n/a>
  */
 
 struct jval {
@@ -39,9 +42,12 @@ struct jval {
 };
 
 
-/*** jval_container ***/
 /*
- * Supports arrays (jval entries) and objects (jval_object_entry entries)
+ * jval_container provides support for container types
+ * (allocated on jval.value.as_container for arrays and objects)
+ * - "arrays" have jval entries
+ * - "objects" have jval_object_entry entries
+ *   (note: duplicate key names permitted)
  */
 
 struct jval_object_entry {
@@ -56,11 +62,33 @@ struct jval_container {
 };
 
 
+/*
+ * aborts on failure, returns self on success
+ */
 struct jval *jval_ensure_type(struct jval *self, enum jtype type);
 
+/*
+ * append requires array, aborts on failure
+ * array steals ownership of value
+ */
 void jval_append(struct jval *self, struct jval *value);
+
+/*
+ * set requires object, aborts on failure
+ * object steals ownership of value
+ */
 void jval_set(struct jval *self, const char *name, struct jval *value);
 
+/*
+ * create jvals of any type
+ * - c long -> JTYPE_INTEGER
+ * - c double -> JTYPE_NUMBER
+ * - c bool -> JTYPE_BOOL
+ * - c string -> JTYPE_STRING
+ * - empty JTYPE_ARRAY (append with jval_append)
+ * - empty JTYPE_OBJECT (set with jval_set)
+ * - JTYPE_NULL
+ */
 struct jval *jval_from_long(long value);
 struct jval *jval_from_double(double value);
 struct jval *jval_from_bool(int value);
@@ -69,6 +97,9 @@ struct jval *jval_new_array(void);
 struct jval *jval_new_object(void);
 struct jval *jval_new_null(void);
 
+/*
+ * destroy any jval recursively
+ */
 void jval_destroy(struct jval *self);
 
 #endif
