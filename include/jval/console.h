ac3acc3c5162b2baecdd5a57a599d7dbc6f1647c
Andrew Palmer <atp@sdf.org>
2022-01-14 05:45:52 -0600

    Output to filehandle

--- include/jval/console.h
+++ include/jval/console.h
@@ -1,6 +1,7 @@
 #ifndef JVAL_CONSOLE_H
 #define JVAL_CONSOLE_H
 
+void jval_out(struct jval *self, FILE *out);
 void jval_to_console(struct jval *self);
 
 #endif
--- src/console.c
+++ src/console.c
@@ -2,63 +2,68 @@
 #include "jval/console.h"
 
 
-static void _array_container_to_console(struct jval_container *self)
+static void _array_container_out(struct jval_container *self, FILE *out)
 {
-    printf("[");
+    fprintf(out, "[");
     if (self->count) {
         size_t curr = 0;
         for (;;) {
-            jval_to_console(self->entries[curr]);
+            jval_out(self->entries[curr], out);
             ++curr;
             if (curr == self->count)
                 break;
-            printf(",");
+            fprintf(out, ",");
         }
     }
-    printf("]");
+    fprintf(out, "]");
 }
 
-static void _object_container_to_console(struct jval_container *self)
+static void _object_container_out(struct jval_container *self, FILE *out)
 {
-    printf("{");
+    fprintf(out, "{");
     if (self->count) {
         size_t curr = 0;
         for (;;) {
             struct jval_object_entry *entry = self->entries[curr];
-            printf("\"%s\":", entry->name);
-            jval_to_console(entry->value);
+            fprintf(out, "\"%s\":", entry->name);
+            jval_out(entry->value, out);
             ++curr;
             if (curr == self->count)
                 break;
-            printf(",");
+            fprintf(out, ",");
         }
     }
-    printf("}");
+    fprintf(out, "}");
+}
+
+void jval_out(struct jval *self, FILE *out)
+{
+    switch (self->type) {
+    case JVAL_TP_INTEGER:
+        fprintf(out, "%ld", self->value.as_long);
+        break;
+    case JVAL_TP_NUMBER:
+        fprintf(out, "%f", self->value.as_double);
+        break;
+    case JVAL_TP_BOOL:
+        fprintf(out, "%s", self->value.as_bool ? "true" : "false");
+        break;
+    case JVAL_TP_STRING:
+        fprintf(out, "\"%s\"", self->value.as_string);
+        break;
+    case JVAL_TP_ARRAY:
+        _array_container_out(self->value.as_container, out);
+        break;
+    case JVAL_TP_OBJECT:
+        _object_container_out(self->value.as_container, out);
+        break;
+    case JVAL_TP_NULL:
+        fprintf(out, "null");
+        break;
+    };
 }
 
 void jval_to_console(struct jval *self)
 {
-    switch (self->type) {
-    case JVAL_TP_INTEGER:
-        printf("%ld", self->value.as_long);
-        break;
-    case JVAL_TP_NUMBER:
-        printf("%f", self->value.as_double);
-        break;
-    case JVAL_TP_BOOL:
-        printf("%s", self->value.as_bool ? "true" : "false");
-        break;
-    case JVAL_TP_STRING:
-        printf("\"%s\"", self->value.as_string);
-        break;
-    case JVAL_TP_ARRAY:
-        _array_container_to_console(self->value.as_container);
-        break;
-    case JVAL_TP_OBJECT:
-        _object_container_to_console(self->value.as_container);
-        break;
-    case JVAL_TP_NULL:
-        printf("null");
-        break;
-    };
+    jval_out(self, stdout);
 }
